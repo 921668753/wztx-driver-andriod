1d79aa9e9dfb2291d071617529784552201b16c3
921668753 <921668753@qq.com>
2018-01-23 22:17:39 +0800

    fix BUG

--- ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/mission/FillOutExpressActivity.java
+++ ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/mission/FillOutExpressActivity.java
@@ -1,7 +1,12 @@
 package com.ruitukeji.zwbs.mission;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.ruitukeji.zwbs.R;
 import com.ruitukeji.zwbs.common.BaseActivity;
+import com.ruitukeji.zwbs.common.BindView;
 import com.ruitukeji.zwbs.utils.ActivityTitleUtils;
 
 /**
@@ -9,8 +14,44 @@ import com.ruitukeji.zwbs.utils.ActivityTitleUtils;
  * Created by Administrator on 2017/11/29.
  */
 
-public class FillOutExpressActivity extends BaseActivity {
+public class FillOutExpressActivity extends BaseActivity implements FillOutExpressContract.View {
 
+    /**
+     * 签收单邮寄地址
+     */
+    @BindView(id = R.id.tv_mailingAddress)
+    private TextView tv_mailingAddress;
+
+    /**
+     * 签收单收件人
+     */
+    @BindView(id = R.id.tv_recipient)
+    private TextView tv_recipient;
+
+    /**
+     * 签收单联系方式
+     */
+    @BindView(id = R.id.tv_contactInformation)
+    private TextView tv_contactInformation;
+
+    /**
+     * 签收单快递单号
+     */
+    @BindView(id = R.id.et_expressNumber)
+    private EditText et_expressNumber;
+
+    /**
+     * 签收单快递公司
+     */
+    @BindView(id = R.id.et_expressCompany)
+    private EditText et_expressCompany;
+
+    /**
+     * 完成订单
+     */
+    @BindView(id = R.id.tv_completeOrder, click = true)
+    private TextView tv_completeOrder;
+    private int g_id = 0;
 
     @Override
     public void setRootView() {
@@ -20,6 +61,8 @@ public class FillOutExpressActivity extends BaseActivity {
     @Override
     public void initData() {
         super.initData();
+        mPresenter = new FillOutExpressPresenter(this);
+        g_id = getIntent().getIntExtra("", 0);
     }
 
 
@@ -27,5 +70,39 @@ public class FillOutExpressActivity extends BaseActivity {
     public void initWidget() {
         super.initWidget();
         ActivityTitleUtils.initToolbar(aty, getString(R.string.fillOutExpress), true, R.id.titlebar);
+
+
+    }
+
+
+    @Override
+    public void widgetClick(View v) {
+        super.widgetClick(v);
+        switch (v.getId()) {
+            case R.id.tv_completeOrder:
+                showLoadingDialog(getString(R.string.submissionLoad));
+                ((FillOutExpressContract.Presenter) mPresenter).postFillOutExpress(g_id, et_expressNumber.getText().toString().trim(), et_expressCompany.getText().toString().trim());
+                break;
+        }
+    }
+
+    @Override
+    public void setPresenter(FillOutExpressContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public void getSuccess(String success, int flag) {
+
+        finish();
+        dismissLoadingDialog();
+    }
+
+    @Override
+    public void errorMsg(String msg, int flag) {
+        dismissLoadingDialog();
+        if (!toLigon1(msg)) {
+            return;
+        }
     }
 }
--- ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/mission/UploadReceiptVoucherActivity.java
+++ ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/mission/UploadReceiptVoucherActivity.java
@@ -1,7 +1,12 @@
 package com.ruitukeji.zwbs.mission;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.ruitukeji.zwbs.R;
 import com.ruitukeji.zwbs.common.BaseActivity;
+import com.ruitukeji.zwbs.common.BindView;
 import com.ruitukeji.zwbs.utils.ActivityTitleUtils;
 
 /**
@@ -9,8 +14,20 @@ import com.ruitukeji.zwbs.utils.ActivityTitleUtils;
  * Created by Administrator on 2017/11/29.
  */
 
-public class UploadReceiptVoucherActivity extends BaseActivity {
+public class UploadReceiptVoucherActivity extends BaseActivity implements UploadReceiptVoucherContract.View {
 
+    /**
+     * 上传图片
+     */
+    @BindView(id = R.id.img_uploadPictures, click = true)
+    private ImageView img_uploadPictures;
+
+
+    /**
+     * 完成订单
+     */
+    @BindView(id = R.id.tv_submit, click = true)
+    private TextView tv_submit;
 
 
     @Override
@@ -28,5 +45,44 @@ public class UploadReceiptVoucherActivity extends BaseActivity {
     public void initWidget() {
         super.initWidget();
         ActivityTitleUtils.initToolbar(aty, getString(R.string.uploadReceiptVoucher), true, R.id.titlebar);
+        if (true) {
+            tv_submit.setText(getString(R.string.submit));
+        } else {
+            tv_submit.setText(getString(R.string.completeOrder));
+        }
+    }
+
+    @Override
+    public void widgetClick(View v) {
+        super.widgetClick(v);
+        switch (v.getId()) {
+            case R.id.img_uploadPictures:
+                showLoadingDialog(getString(R.string.crossLoad));
+
+                break;
+            case R.id.tv_submit:
+                showLoadingDialog(getString(R.string.submissionLoad));
+
+                break;
+        }
+    }
+
+
+    @Override
+    public void setPresenter(UploadReceiptVoucherContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public void getSuccess(String success, int flag) {
+        dismissLoadingDialog();
+    }
+
+    @Override
+    public void errorMsg(String msg, int flag) {
+        dismissLoadingDialog();
+        if (!toLigon1(msg)) {
+            return;
+        }
     }
 }
