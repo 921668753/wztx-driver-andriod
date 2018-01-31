6f9545895bd895459a6b51474bd35927ea720336
921668753 <921668753@qq.com>
2018-01-31 14:30:09 +0800

    fix BUG

--- ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/main/MineFragment.java
+++ ZWBS-Android/app/src/main/java/com/ruitukeji/zwbs/main/MineFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
